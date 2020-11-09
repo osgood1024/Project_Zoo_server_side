387df0b32c4081f0edaa555da030a5ffd718fd60
osgood gunawan <ogunawan@ucsd.edu>
2020-11-08 23:49:28 -0500

    rails db update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_14_155407) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
